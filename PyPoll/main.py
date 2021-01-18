aa18a0f8003ee871276c349ee7ba2b1fe6886fdd
JBGA21 <jesusbeltran_93@hotmail.com>
2021-01-17 18:57:35 -0600

    Final output done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,7 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 
 vote_count = 0
 candidates = {}
+vote_percent = []
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
@@ -15,20 +16,14 @@ with open(csvpath, 'r') as csvfile:
         if row[2] not in candidates:
             candidates[row[2]] = 1
         else:
-            candidates[row[2]] +=1
+            candidates[row[2]] += 1
 
 print(f'Election Results \n-------------------------\nTotal Votes: {vote_count}\n-------------------------')
 
-khan_prop = candidates['Khan']/vote_count
-correy_prop = candidates['Correy']/vote_count
-li_prop = candidates['Li']/vote_count
-o_tooley_prop = candidates['O\'Tooley']/vote_count
+for key, value in candidates.items():
+    cand_props = candidates[key]/vote_count
+    cand_percent = "{:.3%}".format(cand_props)
+    candidates[key] = [cand_percent, value]
 
-khan_percent = "{:.3%}".format(khan_prop)
-correy_percent = "{:.3%}".format(correy_prop)
-li_percent = "{:.3%}".format(li_prop)
-o_tooley_percent = "{:.3%}".format(o_tooley_prop)
-
-for keys, values in candidates.items():
-    print(keys, values)
-
+for keys, value in candidates.items():
+    print(f'{keys}: {candidates[keys][0]} ({candidates[keys][1]})')
