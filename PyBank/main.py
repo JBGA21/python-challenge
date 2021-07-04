bce7ecbac8bad175ce50f7ec13863ba6b6371923
JBGA21 <jesusbeltran_93@hotmail.com>
2021-07-04 14:46:16 -0500

    main.py updated

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,50 +1,74 @@
-# Import requeried libaries
 import os
 import csv
 
-# Write down the file path of the csv
-csvpath = os.path.join('Resources', 'budget_data.csv')
+# save the csv path
+csvpath = os.path.join("Resources", "budget_data.csv")
 
+# read the csv and perform the analysis
+with open(csvpath) as csvfile:
+    # use csv reader to read the csvfile
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-month_counter = 0
-total_amount = 0
-pl_values = []
-months = []
-changes = []
-
-
-with open(csvpath, 'r') as csv_file:
-    csvreader = csv.reader(csv_file)
+    # save the header in order to separate the data you want to use for performing the analysis
     header = next(csvreader)
 
+    # varaibles to save calculations
+    total_months = 0
+    net_amount = 0
+    values = []
+    dates = []
+
+    # iterate through the csv
     for row in csvreader:
-        month_counter += 1
-        total_amount += int(row[1])
-        pl_values.append(int(row[1]))
-        months.append(row[0])
+        total_months += 1
+        net_amount += int(row[1])
+        values.append(row[1])
+        dates.append(row[0])
+    # variables to calculate change
+    i = 0
+    changes = []
+
+    # loop through the profit/loss values
+    for x in values:
+        if i == 0:
+            pass
+        else:
+            # calculate the change
+            change = int(values[i]) - int(values[i-1])
+            changes.append(change)
+
+        # update the counter variable
+        i += 1
+
+    # calculate the average change
+    avg_change = round(sum(changes)/len(changes), 2)
+
+    # calculate the greatest increase in profits
+    # get the index of the max change
+    max_index = changes.index(max(changes))
+
+    # get the max value in changes
+    max_change = max(changes)
+
+    # get the date of the max change
+    max_date = dates[24+1]
+
+    # calculate the greatest decrease in profits
+    # get the index of the min change
+    min_index = changes.index(min(changes))
+
+    # get the min value in changes
+    min_change = min(changes)
+
+    # get the date of the min change
+    min_date = dates[43+1]
 
 
-for i in range(len(pl_values)):
-    if i == 0:
-        continue
-    change = pl_values[i] - pl_values[i-1]
-    changes.append(change)
-
-average_change = sum(changes) / len(changes)
-
-max_value = max(changes)
-max_index = changes.index(max_value)
-max_month = months[max_index + 1]
-
-min_value = min(changes)
-min_index = changes.index(min_value)
-min_month = months[min_index + 1]
-
-
-print('Financial Analysis')
-print('----------------------------')
-print(f'Total Months: {month_counter}')
-print(f'Total: ${total_amount}')
-print(f'Average Change: ${average_change}')
-print(f'Greatest Increase in Profits: {max_month} (${max_value})')
-print(f'Greatest Decrease in Profits: {min_month} (${min_value})')
+# print the results
+print("Financial Analysis")
+print("----------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_amount}")
+print(f"Average Change ${avg_change}")
+print(f"Greatest Increase in Profits: {max_date} (${max_change})")
+print(f"Greatest Decrease in Profits: {min_date} (${min_change})")
